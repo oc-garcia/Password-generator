b8772c45ae562026f58f9cb62fca666a84f37b28
oc-garcia <octaviogarcia1988@gmail.com>
2022-12-27 14:24:22 -0300

    advancing in JS

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -63,3 +63,8 @@ function strenghtBar() {
     }
   }
 }
+
+let uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let symbolsArray = ["!", "#", "$", "%", "&", "*", "@"]
